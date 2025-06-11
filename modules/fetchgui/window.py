2f1e6df5b5237f35c5ee814cc1e9749123f576d7
LostLukas <lukasdorji@gmail.com>
2025-06-11 17:25:16 +0200

    change(fetchgui): flesh out the ui move to QMainWindow and add a about dialog

--- modules/fetchgui/window.py
+++ modules/fetchgui/window.py
@@ -1,28 +1,80 @@
 
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QLayout, QPushButton, QCheckBox, QWidget, QLabel, QPlainTextEdit
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMainWindow, QMessageBox, QVBoxLayout, QHBoxLayout, QLayout, QPushButton, QCheckBox, QLabel, QPlainTextEdit, QWidget
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("SystemInfoPdf")
+        self.application_name = "SystemInfo Report"
+        self.version = "0.2.0"
+        self.setWindowTitle(self.application_name)
+
+        self.setup_ui()
+        self.setup_menu()
+
+    def setup_ui(self):
 
         # Create Widgets
         options_label = QLabel("Optionen:")
-        start_button = QPushButton("Start")
-        log_textbox = QPlainTextEdit()
-        log_textbox.setReadOnly(True)
+
+        self.start_button = QPushButton("Start")
+        self.start_button.pressed.connect(self.start_fetching)
+
+        self.log_textbox = QPlainTextEdit()
+        self.log_textbox.setReadOnly(True)
 
         # Setup Layout
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
+        options_vbox = QVBoxLayout()
+
+            # Options
+        options_vbox.addWidget(options_label)
+
+        general_info_hbox, self.general_info_checkbox = self.make_option(True, "Allgemeine Systeminformationen")
+        options_vbox.addLayout(general_info_hbox)
+
+        disk_info_hbox, self.disk_info_checkbox = self.make_option(True, "Speichergeräteinformationen")
+        options_vbox.addLayout(disk_info_hbox)
+
+        options_vbox.addStretch()
 
         vbox.addLayout(hbox)
-        hbox.addWidget(options_label)
-        hbox.addWidget(log_textbox)
+        hbox.addLayout(options_vbox)
+        hbox.addWidget(self.log_textbox)
 
-        vbox.addWidget(start_button)
+        vbox.addWidget(self.start_button)
 
         # Set Layout
-        self.setLayout(vbox)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        central_widget.setLayout(vbox)
+
+    def make_option(self, checked: bool, option_text: str) -> tuple[QHBoxLayout, QCheckBox]:
+        checkbox = QCheckBox()
+        checkbox.setChecked(checked)
+        label = QLabel(option_text)
+
+        layout = QHBoxLayout()
+        layout.addWidget(checkbox)
+        layout.addWidget(label)
+        layout.addStretch()
+        return layout, checkbox
+
+    def setup_menu(self):
+        menu_bar = self.menuBar()
+
+        help_menu = menu_bar.addMenu("Hilfe")
+
+        about_action = QAction("Über", self)
+        about_action.triggered.connect(self.show_about_dialog)
+
+        help_menu_actions: list[QAction] = [about_action]
+        help_menu.addActions(help_menu_actions)
+
+    def show_about_dialog(self):
+        QMessageBox.about(self, f"Über {self.application_name}", f"{self.application_name}\nVersion {self.version}\nErstellt von Lukas Dorji")
 
 
+    def start_fetching(self):
+        self.log_textbox.appendPlainText("Gestartet")
