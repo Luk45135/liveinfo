3a7be4500eb8dbb59fd54d740fee1408aefcd166
LostLukas <lukasdorji@gmail.com>
2025-06-12 10:17:07 +0200

    add(gui): define application icon

--- modules/fetchscript/src/main.py
+++ modules/fetchscript/src/main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import os
 import sys
+from pathlib import Path
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from fetchscript.window import Window
 
 def main():
     app = QApplication(sys.argv)
+
+    icon_path = Path(os.environ.get("FETCHSCRIPT_SHARE", "../share/fetchscript/")) / "search-list.png"
+    app.setWindowIcon(QIcon(str(icon_path.resolve())))
     
     window = Window()
     window.show()
