8959cf9d10f94ea7a965043dbcee704408b359a5
LostLukas <lukasdorji@gmail.com>
2025-06-30 12:31:38 +0200

    add(main.py): add comments

--- modules/fetchscript/src/main.py
+++ modules/fetchscript/src/main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# This file is the entry point for the app
+
 
 import os
 import sys
@@ -8,15 +10,20 @@ from PySide6.QtWidgets import QApplication
 from fetchscript.window import Window
 
 def main():
+    # This creates the application for the Qt GUI
     app = QApplication(sys.argv)
 
+    # This gets the path of the application icon and sets it
     icon_path = Path(os.environ.get("FETCHSCRIPT_SHARE", "../share/fetchscript/")) / "search-list.png"
     app.setWindowIcon(QIcon(str(icon_path.resolve())))
     
+    # This instantiates the Window object from window.py and then shows it
     window = Window()
     window.show()
     
+    # Boilerplate to handle graceful exit
     sys.exit(app.exec())
 
+# Run the main function when this file gets executed directly
 if __name__ == "__main__":
     main()
